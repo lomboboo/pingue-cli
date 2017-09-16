6ec28d31af9a573afccb168ef85c57b9af618523
lomboboo <lomboboo@gmail.com>
2017-09-16 15:03:07 +0200

    Task: Create

--- config.js
+++ config.js
@@ -3,9 +3,14 @@ const repo = 'https://github.com/lomboboo/webpack-typescript-boilerplate.git';
 const messages = {
 	create: {
 		start: 'Creating new project, please wait...',
+		git_remove: 'Git diretory removed.',
+		git_clone_start: 'Cloning repository...',
+		git_clone_succeed: 'Repository has been cloned',
 		directory_exists: `already exists`,
-		finished: (project_name) => `SUCCESS! New project ${project_name} has been created.`,
-		npm_failed: 'Package install failed, see above.'
+		finished: ( project_name ) => `SUCCESS! New project ${project_name} has been created.`,
+		npm_start: 'Installing dependencies...',
+		npm_failed: 'Package install failed, see above.',
+		npm_finished: 'Installed packages for tooling via npm.'
 	}
 };
 
--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const chalk       = require('chalk');
-const clear       = require('clear');
-const figlet      = require('figlet');
-const inquirer    = require('inquirer');
-const Preferences = require('preferences');
-const _           = require('lodash');
-const fs          = require('fs');
-const program = require('commander');
+const chalk = require( 'chalk' );
+const clear = require( 'clear' );
+const figlet = require( 'figlet' );
+const inquirer = require( 'inquirer' );
+const Preferences = require( 'preferences' );
+const _ = require( 'lodash' );
+const fs = require( 'fs' );
+const program = require( 'commander' );
 
-const { create } = require('./lib/commands/new');
+const { create } = require( './lib/commands/new' );
 
 let init = () => {
 
 	console.log(
 		chalk.green(
-			figlet.textSync('Pingue CLI', { horizontalLayout: 'full' })
+			figlet.textSync( 'Pingue CLI', { horizontalLayout: 'full' } )
 		)
 	);
 
 	program
-		.version('1.0.0')
-		.description('Pingue CLI fast up&run boilerplate');
+		.version( '1.0.0' )
+		.description( 'Pingue CLI fast up&run boilerplate' );
 
 	program
-		.command('new <project_name>')
-		.alias('n')
-		.description('Create new project')
-		.action((project_name) => {
-			create(project_name);
-		});
+		.command( 'new <project_name>' )
+		.alias( 'n' )
+		.description( 'Create new project' )
+		.action( ( project_name ) => {
+			create( project_name );
+		} );
 
-	program.parse(process.argv);
+	program.parse( process.argv );
 
 };
 
